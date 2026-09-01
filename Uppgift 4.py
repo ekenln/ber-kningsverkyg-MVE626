58aaa4eab474c5655b54fbb4dae89d1c47752a93
Gustav Karlberg <gustav.sjuhalla@gmail.com>
2026-09-01 15:19:27 +0200

    Uppgift 4

---
+++ Uppgift 4.py
@@ -0,0 +1,20 @@
+import math
+
+x = float(input("x = "))
+y = float(input("y = "))
+
+r = math.sqrt(x**2 + y**2)
+
+if x>=0:
+    p = math.atan(y/x)
+elif y>=0:
+    p = math.pi + math.atan(y/x)
+elif y<0:
+    p = math.pi + math.atan(y/x)
+
+if p<0:
+    p+=2*math.pi
+
+p = math.degrees(p)
+
+print("radie = ", r, ", vinkel = ", p, "°")
