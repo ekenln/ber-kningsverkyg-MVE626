6e60c79340d59d5d42b88a69afc3f47dbbe1f51e
ellen eklund <elleneklund@MacBook-Air-69>
2026-09-07 15:02:30 +0200

    fixed fraga 3

--- labb2/inClass.py
+++ labb2/inClass.py
@@ -14,42 +14,39 @@
 
 #FRÅGA 2
 
-import random
+# import random
 
-antal = int(input("mata in positivt heltal: "))
+# antal = int(input("mata in positivt heltal: "))
 
-random_list = []
-s = 0
+# random_list = []
+# s = 0
 
 
-for i in range(antal):
-	rand_num = random.randint(1,100)
-	random_list.append(rand_num)
-	print(f"Tal nr {i+1} är {rand_num}")
-	s += rand_num
+# for i in range(antal):
+# 	rand_num = random.randint(1,100)
+# 	random_list.append(rand_num)
+# 	print(f"Tal nr {i+1} är {rand_num}")
+# 	s += rand_num
 
-m = s/antal
-print("summan är {s}")
-print("medelvärdet är", round(m,2))
+# m = s/antal
+# print("summan är {s}")
+# print("medelvärdet är", round(m,2))
 
 
 # FRÅGA 3
-# import random 
+import random 
 
-# A = set({})
-# B = set({})
+A = set({})
+B = set({})
 
-# while len(A) < 20:
-# 	A.add(random.randint(1,100))
+while len(A) < 20:
+	A.add(random.randint(1,100))
 
-# while len(B) < 20:
-# 	B.add(random.randint(1,100))
+while len(B) < 20:
+	B.add(random.randint(1,100))
 
-# print("A: ", A)
-# print("\nB: ",B)
-
-# print("Unionen av A och B är", A.union(B))
-# print("Snittet av A och B är", A.intersection(B))
+print("Antal element i unionen av A och B är", len(A.union(B)))
+print("Antalet element i snittet av A och B är", len(A.intersection(B)))
 
 
 
